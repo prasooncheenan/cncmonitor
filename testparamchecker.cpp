109d3d171f942d4658b023a15f83b8b03a88ad56
prasooncheenan <prasooncc@gmail.com>
2020-08-07 21:24:09 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -5,9 +5,7 @@ TEST(VitalsTest, BPM) {
     ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
 }
  
-TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
-}
+
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
