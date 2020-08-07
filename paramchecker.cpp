9f355bd11eb9439cdb5f0e13bf539225d16e9d93
prasooncheenan <prasooncc@gmail.com>
2020-08-07 20:24:47 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -2,25 +2,43 @@
 
 #include <iostream>
 
+uint8_t Connect(std::function<void(int, double)>)
+{
+    return 255;
+}
 
-
-
-
-
-bool isWithinLimits(float input, std::pair<float, float> limits)
+bool isWithinLimits(double input, std::pair<double, double> limits)
 {
     if (input < limits.first || input > limits.second)
         return false;
     return true;
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-    HealthResult hrResult;
-    Limits allLimits;
-    hrResult.AddResult(BPM,isWithinLimits(bpm, allLimits.GetLimits(BPM)));
-    hrResult.AddResult(SPO, isWithinLimits(spo2, allLimits.GetLimits(SPO)));
-    hrResult.AddResult(RESP, isWithinLimits(respRate, allLimits.GetLimits(RESP)));
-
-    if ((bool)(hrResult)!= true) return false;
+    
 
     return true;
+}
+
+std::string CheckLimits(int type, double value)
+{
+    std::string Update = "";
+    CurrentParameters hrResult;
+    Limits allLimits;
+    if (type == TOTALTIME)
+        hrResult.AddValue(TOTALTIME, value);
+    else
+    hrResult.SetValue(type, value);
+    if (!isWithinLimits(value, allLimits.GetLimits(type)))
+    {
+        return InformationRunning[type];
+    }
+
+    return Update;
+}
+
+void GetMachineDataCallback(int type, double value)
+{
+   auto info =  CheckLimits(type, value);
+   if (!info.empty())
+       std::cout << info;
 }
